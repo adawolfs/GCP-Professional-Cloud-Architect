e47bdc3663e5735b0c805ae8b5772e066d4fc99e
adawolfs <adawolfs@gmail.com>
2022-09-09 20:42:43 -0600

    Fix compute creation
    Change region, zone, image

--- compute/server.tf
+++ compute/server.tf
@@ -3,7 +3,7 @@
 provider "google" {
   credentials = "${file("credentials.json")}"
   project     = "bridge-cloud-362101"
-  region      = "us-central1"
+  region      = "us-east1"
 }
   
 
@@ -11,11 +11,11 @@ resource "google_compute_instance" "default" {
   name         = "rustedchipmunk"
 
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  zone         = "us-east1-b"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-cloud/centos-stream-8-v20220822"
     }
   }
 
